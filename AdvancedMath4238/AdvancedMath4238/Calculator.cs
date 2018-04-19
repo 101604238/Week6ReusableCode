e500cc4877d16ade81e569a31664535ab859a951
Nicholas Hicks <nicholasmhicks@gmail.com>
2018-04-20 08:27:38 +1000

    My First Comment

---
+++ AdvancedMath4238/AdvancedMath4238/Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedMath4238
+{
+    public class Calculator
+    {
+        public int Factorial(int input)
+        {
+            if (input > 1)
+            {
+                return input * Factorial(input - 1);
+            }
+            else
+            {
+                return 1;
+            }
+        }
+    }
+}
---
+++ DLLTest4238/DLLTest4238/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdvancedMath4238;
+
+namespace DLLTest4238
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter an integer");
+            var input = int.Parse(Console.ReadLine());
+
+            var calculator = new Calculator();
+
+            var result = calculator.Factorial(input);
+
+            Console.WriteLine("The Factorial of " + input.ToString() + " is " + result.ToString());
+            Console.ReadKey();
+        }
+    }
+}
